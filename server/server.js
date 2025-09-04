12c9b90cd57c8d73003585e6af097129e64e7d32
Bryan_w11 <h.bryan68@yahoo.ca>
2025-09-04 17:26:14 -0400

    server set up

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./db");
+
+require("dotenv").config();
+
+const app = express();
+// Connect to database
+
+app.use(cors());
+app.use(express.json());
+
+// Log to confirm API running on root
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT;
+// Start Express server
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
