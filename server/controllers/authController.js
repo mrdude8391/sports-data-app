334a0725df7e2a24273d0ae6447bf808b477b6c7
Bryan_w11 <h.bryan68@yahoo.ca>
2025-09-08 15:52:27 -0400

    create register user controller

---
+++ server/controllers/authController.js
@@ -0,0 +1,42 @@
+const User = require("./User.js");
+// bcrypt encryption library to help hash passwords
+const bcrypt = require("bcryptjs");
+// jwt -> library to help with working with JSON web tokens: signing, verifying, decoding
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+};
+
+// Register User
+const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    // if user exists return 400
+    const userExists = User.findOne(email);
+    if (!userExists) {
+      res.status(400).json({ message: "Email already in use" });
+    }
+    // hash password
+    const hashedPassword = bcrypt.hash(password);
+    // create user
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    // generate token
+    const token = generateToken(user._id);
+    // return user info and token
+    res.status(201).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      token: token,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = { registerUser };
