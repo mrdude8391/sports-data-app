79ccf027509ef05625509350b0e8bb0cd05eb61a
Bryan_w11 <h.bryan68@yahoo.ca>
2025-09-10 10:16:22 -0400

    authorization middleware to verify the token provided from client. gets the user data without password

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,15 +1,20 @@
 const jwt = require("jsonwebtoken");
-const User = require("..models/User");
+const User = require("../models/User");
 
+// Token verification
 const protect = async (req, res, next) => {
   let token;
+  // Verify the correct headers sent
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
     try {
+      // split the token from the header string
       token = req.headers.authorization.split(" ")[1];
+      // decode the ID from the token using JWT
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      // get the user from the decoded ID, without the password, and attach to the request.user
       req.user = await User.findById(decoded.id).select("-password");
       next();
     } catch (error) {
@@ -21,3 +26,5 @@ const protect = async (req, res, next) => {
     }
   }
 };
+
+module.exports = { protect };
