dbb5b9825a7f1dea922bfc705fc4ae0be5685f42
Bryan_w11 <h.bryan68@yahoo.ca>
2025-09-11 16:38:36 -0400

    login UI

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,31 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <div className="flex flex-col border-1 rounded-2xl p-5 gap-5 min-w-sm">
+        <div className="flex flex-col gap-1">
+          <label>Email</label>
+          <input
+            className="border-1 rounded-lg p-2"
+            placeholder="Email"
+          ></input>
+        </div>
+        <div className="flex flex-col gap-1">
+          <label>Password</label>
+          <input
+            className="border-1 rounded-lg p-2"
+            placeholder="Password"
+          ></input>
+        </div>
+        <div className="flex gap-5 justify-between">
+          <Button className="flex-1">Login</Button>
+          <Button className="flex-1">Sign Up</Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
