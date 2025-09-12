13beb4923f10d0ab87bd385e3a261ee72b31bca7
Bryan_w11 <h.bryan68@yahoo.ca>
2025-09-11 21:20:51 -0400

    implement login button

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,15 +1,27 @@
 import { Button } from "@/components/ui/button";
-import React from "react";
+import React, { useState } from "react";
+import * as sportsDataservice from "../services/sportsDataService";
+import type { User } from "@/types/User";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    let user = await sportsDataservice.login(email, password);
+    console.log("login page", user);
+  };
+
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       <div className="flex flex-col border-1 rounded-2xl p-5 gap-5 min-w-sm">
         <div className="flex flex-col gap-1">
           <label>Email</label>
           <input
             className="border-1 rounded-lg p-2"
             placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
           ></input>
         </div>
         <div className="flex flex-col gap-1">
@@ -17,14 +29,17 @@ const Login = () => {
           <input
             className="border-1 rounded-lg p-2"
             placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
           ></input>
         </div>
         <div className="flex gap-5 justify-between">
-          <Button className="flex-1">Login</Button>
+          <Button className="flex-1" type="submit">
+            Login
+          </Button>
           <Button className="flex-1">Sign Up</Button>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
