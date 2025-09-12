9f9971c8b648656bcd98c78e70cf4cab9a3d8e21
bryan_mac <h.bryan68@yahoo.ca>
2025-09-12 16:27:21 -0400

    create profile page

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,7 +1,22 @@
+import AthleteCard from "@/components/AthleteCard";
+import { Image } from "lucide-react";
+import sampleImage from "../assets/sampleImage.png";
 import React from "react";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="flex gap-5 items-center  rounded-lg px-5 py-2">
+        <img height={48} width={48} src={sampleImage}></img>
+        <div className="px-3 py-1 border-1 rounded-lg">My Profile Data</div>
+      </div>
+      <div className="flex flex-col gap-5 py-10 px-15 border-2 rounded-xl">
+        <AthleteCard imageSrc={sampleImage} athlete="Micaa" />
+        <AthleteCard imageSrc={sampleImage} athlete="Gage" />
+        <AthleteCard imageSrc={sampleImage} athlete="Joe" />
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
