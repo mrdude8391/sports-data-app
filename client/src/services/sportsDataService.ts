a21937fbbf8660f709fc1c3dd249e58437e735d6
Bryan_w11 <h.bryan68@yahoo.ca>
2025-09-11 21:21:06 -0400

    login api service route

--- client/src/services/sportsDataService.ts
+++ client/src/services/sportsDataService.ts
@@ -1,4 +1,21 @@
 import type { User } from './../types/User';
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
-const API_URL = process.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL
+
+export const login = async (email: String, password: String) => {
+    try {
+        const res = await axios.post(API_URL + '/auth/login', {email, password} )
+        const user:User = {
+            username: res.data.username,
+            email: res.data.email,
+            token: res.data.token
+        }
+        return user
+    } catch (error: any) {
+        console.log(error.response.data.message)
+    }
+    
+
+}
+
