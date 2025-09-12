b0d286c3fd07188869a05d114e09edb2177d37fa
bryan_mac <h.bryan68@yahoo.ca>
2025-09-12 15:24:43 -0400

    create profile page and add profile page route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import Login from "./pages/Login.tsx";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./pages/layouts/MainLayout.tsx";
+import Profile from "./pages/Profile.tsx";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
           </Route>
         </Routes>
       </div>
