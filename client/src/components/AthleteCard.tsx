09f41e049f5b4b3e74f8ad1097313ea4213c91c4
bryan_mac <h.bryan68@yahoo.ca>
2025-09-12 16:26:46 -0400

    create ahtlete card component

---
+++ client/src/components/AthleteCard.tsx
@@ -0,0 +1,24 @@
+import { Image } from "lucide-react";
+import React from "react";
+
+interface AthleteCardProps {
+  imageSrc: string;
+  athlete: string;
+}
+
+const AthleteCard = (props: AthleteCardProps) => {
+  const { imageSrc, athlete } = props;
+  return (
+    <div className="flex gap-5 items-center border-1 rounded-lg px-5 py-2">
+      <img
+        src={imageSrc}
+        height={48}
+        width={48}
+        className="border-1 rounded-full p-2"
+      />
+      <div className="px-3 py-1 border-1 rounded-lg">{athlete}</div>
+    </div>
+  );
+};
+
+export default AthleteCard;
