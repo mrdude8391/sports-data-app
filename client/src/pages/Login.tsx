d870f4edb2739e459e74f3225bd2b6d980573e61
Bryan_w11 <h.bryan68@yahoo.ca>
2025-09-15 17:13:38 -0400

    reformat login page to shadcn card

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -3,6 +3,9 @@ import React, { useState } from "react";
 import * as sportsDataservice from "../services/sportsDataService";
 import type { User } from "@/types/User";
 import { Link } from "react-router-dom";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -15,34 +18,48 @@ const Login = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="flex flex-col border-1 rounded-2xl p-5 gap-5 min-w-sm">
-        <div className="flex flex-col gap-1">
-          <label>Email</label>
-          <input
-            className="border-1 rounded-lg p-2"
-            placeholder="Email"
-            onChange={(e) => setEmail(e.target.value)}
-          ></input>
-        </div>
-        <div className="flex flex-col gap-1">
-          <label>Password</label>
-          <input
-            className="border-1 rounded-lg p-2"
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-          ></input>
-        </div>
-        <div className="flex gap-5 justify-between">
-          <Button className="flex-1" type="submit">
+    <>
+      <Card className="w-full max-w-sm">
+        <CardContent>
+          <form id="loginForm" onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  type="email"
+                  placeholder="Email"
+                  id="email"
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
+                ></Input>
+              </div>
+
+              <div className="grid gap-3">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  placeholder="Password"
+                  id="password"
+                  required
+                  onChange={(e) => setPassword(e.target.value)}
+                ></Input>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2 justify-between">
+          <Button
+            className="w-full cursor-pointer"
+            type="submit"
+            form="loginForm"
+          >
             Login
           </Button>
-          <Button className="flex-1">
+          <Button variant="outline" className="w-full">
             <Link to="/register">Sign Up</Link>
           </Button>
-        </div>
-      </div>
-    </form>
+        </CardFooter>
+      </Card>
+    </>
   );
 };
 
