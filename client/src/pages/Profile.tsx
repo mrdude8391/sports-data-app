f8c163472ecd270b9a121a05889250763b49d4c3
Bryan_w11 <h.bryan68@yahoo.ca>
2025-09-15 13:51:54 -0400

    Implemented getprofile function to get profile data from api service and set state variable username if user object is returned. Using react useEffect to call getprofile. Changed profile description to display username.

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,14 +1,29 @@
 import AthleteCard from "@/components/AthleteCard";
 import { Image } from "lucide-react";
 import sampleImage from "../assets/sampleImage.png";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import * as sportsDataService from "../services/sportsDataService";
+import type { User } from "@/types/User";
 
 const Profile = () => {
+  const [username, setUsername] = useState("");
+
+  const getProfile = async () => {
+    const user = await sportsDataService.profile();
+    if (user) {
+      setUsername(user.username);
+    }
+  };
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
   return (
     <div className="flex flex-col gap-2">
       <div className="flex gap-5 items-center  rounded-lg px-5 py-2">
         <img height={48} width={48} src={sampleImage}></img>
-        <div className="px-3 py-1 border-1 rounded-lg">My Profile Data</div>
+        <div className="px-3 py-1 border-1 rounded-lg">{username}</div>
       </div>
       <div className="flex flex-col gap-5 py-10 px-15 border-2 rounded-xl">
         <AthleteCard imageSrc={sampleImage} athlete="Micaa" />
