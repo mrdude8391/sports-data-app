1eed1e91fa3db1ce1576e4a088a555d7dc19bc24
Bryan_w11 <h.bryan68@yahoo.ca>
2025-09-17 16:49:01 -0400

    added register user ui added register user form added back to login page button added start hidden successfully registered div

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Card,
   CardAction,
@@ -9,22 +9,81 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Alert, AlertTitle } from "@/components/ui/alert";
+import { Link } from "react-router-dom";
+import * as sportsDataService from "../services/sportsDataService";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const user = await sportsDataService.register(email, password);
+    console.log(user);
+  };
+
   return (
     <>
       <div className="w-full max-w-sm">
         <Card>
           <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-            <CardAction>Card Action</CardAction>
+            <CardTitle>Register</CardTitle>
+            <CardDescription>
+              Please enter an email and password to sign up
+            </CardDescription>
           </CardHeader>
           <CardContent>
-            <p>Card Content</p>
+            <form id="registerForm" onSubmit={handleSubmit}>
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-3">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    type="email"
+                    placeholder="Email"
+                    id="email"
+                    required
+                    onChange={(e) => setEmail(e.target.value)}
+                  ></Input>
+                </div>
+
+                <div className="grid gap-3">
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    placeholder="Password"
+                    id="password"
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                  ></Input>
+                </div>
+              </div>
+            </form>
           </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
+          <CardFooter className="grid">
+            <div className="flex flex-col gap-2">
+              <Button
+                form="registerForm"
+                type="submit"
+                className="justify-center cursor-pointer"
+              >
+                Register
+              </Button>
+              <Link to="/login" className="w-full">
+                <Button variant="outline" className="w-full cursor-pointer">
+                  Back to Login
+                </Button>
+              </Link>
+              {showAlert && (
+                <Alert>
+                  <AlertTitle className="text-center">
+                    Success! Your have registered your account
+                  </AlertTitle>
+                </Alert>
+              )}
+            </div>
           </CardFooter>
         </Card>
       </div>
