d39cf7db0c1258a5ab823432272814d920602c5b
Bryan_w11 <h.bryan68@yahoo.ca>
2025-09-23 20:36:27 -0400

    created athlete schema

---
+++ server/models/Athete.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const athleteSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+    name: { type: String, required: true },
+    age: { type: Number, required: true },
+    height: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
+
+// Compound index: prevents duplicate athlete names per user
+athleteSchema.index({ userId: 1, name: 1 }, { unique: true });
+
+module.exports = mongoose.model("User", userSchema);
