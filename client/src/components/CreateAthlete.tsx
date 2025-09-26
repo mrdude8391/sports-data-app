7a0e2085c35636ff8cacba498b151356007b9a24
Bryan_w11 <h.bryan68@yahoo.ca>
2025-09-26 16:22:39 -0400

    created create athlete component

---
+++ client/src/components/CreateAthlete.tsx
@@ -0,0 +1,77 @@
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "./ui/button";
+import { useState } from "react";
+import * as sportsDataService from "@/services/sportsDataService";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import type { Athlete } from "@/types/Athlete";
+
+const CreateAthlete = () => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [height, setHeight] = useState("");
+
+  const queryClient = useQueryClient();
+
+  const { isPending, error, mutate } = useMutation({
+    mutationFn: sportsDataService.createAthlete,
+    onSuccess: (newAthlete) => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries({ queryKey: ["athletes"] });
+    },
+  });
+
+  if (isPending) return <div>pending</div>;
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const athlete: Athlete = {
+      name: name,
+      age: Number(age),
+      height: Number(height),
+    };
+    mutate(athlete);
+  };
+  return (
+    <div>
+      {error && <div>{error.message}</div>}
+      <form id="athleteForm" onSubmit={handleSubmit}>
+        <Label htmlFor="name">Name</Label>
+        <Input
+          type="text"
+          id="name"
+          value={name}
+          placeholder="Name"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        ></Input>
+        <Label htmlFor="age">age</Label>
+        <Input
+          type="number"
+          id="age"
+          value={age}
+          placeholder="Age"
+          onChange={(e) => {
+            setAge(e.target.value);
+          }}
+        ></Input>
+        <Label htmlFor="height">height</Label>
+        <Input
+          type="number"
+          id="height"
+          value={height}
+          placeholder="Height"
+          onChange={(e) => {
+            setHeight(e.target.value);
+          }}
+        ></Input>
+      </form>
+      <Button type="submit" form="athleteForm">
+        Create Athlete
+      </Button>
+    </div>
+  );
+};
+
+export default CreateAthlete;
