8423b4a805025d2b2e1e454da5caf67393e56049
Bryan_w11 <h.bryan68@yahoo.ca>
2025-09-26 16:23:22 -0400

    chnaged the athlete function to use tanstack query. added create athlete component on athlete page

--- client/src/pages/Athletes.tsx
+++ client/src/pages/Athletes.tsx
@@ -5,46 +5,75 @@ import * as sportsDataService from "../services/sportsDataService";
 import sampleImage from "../assets/circle-user-round.svg";
 import { useAuth } from "@/context/AuthContext";
 import { Loader } from "lucide-react";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import CreateAthlete from "@/components/CreateAthlete";
 
 const Athletes = () => {
-  const [athletes, setAthletes] = useState<Athlete[]>([]);
-  const [loading, setLoading] = useState(true);
+  // const [athletes, setAthletes] = useState<Athlete[]>([]);
+  // const [loading, setLoading] = useState(true);
 
-  const { isLoggedIn } = useAuth();
+  // const { isLoggedIn } = useAuth();
 
-  useEffect(() => {
-    const fetchAthletes = async () => {
-      try {
-        setLoading(true);
-        const res = await sportsDataService.getAthletes();
-        setAthletes(res);
-      } catch (error) {
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchAthletes();
-    console.log("fetch athletes", athletes);
-  }, []);
+  // const fetchAthletes = async () => {
+  //   try {
+  //     setLoading(true);
+  //     const athletes = await sportsDataService.getAthletes();
+  //     // setAthletes(res);
+  //     return athletes
+  //   } catch (error) {
+  //     console.log(error)
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchAthletes();
+  //   console.log("fetch athletes", athletes);
+  // }, []);
+
+  //  return (
+  //   <div className="flex flex-col gap-5">
+  //     {isLoggedIn ? (
+  //       loading ? (
+  //         <Loader className="animate-spin" />
+  //       ) : (
+  //         athletes.map((athlete) => (
+  //           <AthleteCard
+  //             key={athlete._id}
+  //             imageSrc={sampleImage}
+  //             athlete={athlete.name}
+  //           />
+  //         ))
+  //       )
+  //     ) : (
+  //       <AthleteCard imageSrc={sampleImage} athlete="No user logged in" />
+  //     )}
+
+  //     <div className="min-w-lg min-h-10 border-2"></div>
+  //   </div>
+  // );
+
+  const {
+    data: athletes,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["athletes"],
+    queryFn: sportsDataService.getAthletes,
+  });
+
+  if (isLoading) return <Loader className="animate-spin" />;
+  if (error)
+    return <AthleteCard imageSrc={sampleImage} athlete="No user logged in" />;
+
   return (
     <div className="flex flex-col gap-5">
-      {isLoggedIn ? (
-        loading ? (
-          <Loader className="animate-spin" />
-        ) : (
-          athletes.map((athlete) => (
-            <AthleteCard
-              key={athlete._id}
-              imageSrc={sampleImage}
-              athlete={athlete.name}
-            />
-          ))
-        )
-      ) : (
-        <AthleteCard imageSrc={sampleImage} athlete="No user logged in" />
-      )}
-
-      <div className="min-w-lg min-h-10 border-2"></div>
+      <CreateAthlete></CreateAthlete>
+      {athletes &&
+        athletes.map((a: Athlete) => (
+          <AthleteCard key={a._id} imageSrc={sampleImage} athlete={a.name} />
+        ))}
     </div>
   );
 };
