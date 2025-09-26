500222bc82ac33b4195dede37e241b2f362e5f36
Bryan_w11 <h.bryan68@yahoo.ca>
2025-09-26 10:01:05 -0400

    Athletes page added fetch athletes function added loading state manager for fetch functiong ui shows loading when loading and shows athlete cards when loaded. added not logged in ui when no user is logged in

--- client/src/pages/Athletes.tsx
+++ client/src/pages/Athletes.tsx
@@ -1,10 +1,49 @@
 import AthleteCard from "@/components/AthleteCard";
-import React from "react";
+import { useEffect, useState } from "react";
+import type { Athlete } from "@/types/Athlete";
+import * as sportsDataService from "../services/sportsDataService";
+import sampleImage from "../assets/circle-user-round.svg";
+import { useAuth } from "@/context/AuthContext";
+import { Loader } from "lucide-react";
 
 const Athletes = () => {
+  const [athletes, setAthletes] = useState<Athlete[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const { isLoggedIn } = useAuth();
+
+  useEffect(() => {
+    const fetchAthletes = async () => {
+      try {
+        setLoading(true);
+        const res = await sportsDataService.getAthletes();
+        setAthletes(res);
+      } catch (error) {
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchAthletes();
+    console.log("fetch athletes", athletes);
+  }, []);
   return (
     <div className="flex flex-col gap-5">
-      <AthleteCard imageSrc="" athlete="" />
+      {isLoggedIn ? (
+        loading ? (
+          <Loader className="animate-spin" />
+        ) : (
+          athletes.map((athlete) => (
+            <AthleteCard
+              key={athlete._id}
+              imageSrc={sampleImage}
+              athlete={athlete.name}
+            />
+          ))
+        )
+      ) : (
+        <AthleteCard imageSrc={sampleImage} athlete="No user logged in" />
+      )}
+
       <div className="min-w-lg min-h-10 border-2"></div>
     </div>
   );
