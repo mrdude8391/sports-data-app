79104f27ce374add2847624a2f49c2709cbe3e1f
Bryan_w11 <h.bryan68@yahoo.ca>
2025-09-29 18:11:52 -0400

    created athletes stat page. moved the create stat button and stat list to a separate component. get stats using react query

--- client/src/pages/AthleteStats.tsx
+++ client/src/pages/AthleteStats.tsx
@@ -1,22 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import * as sportsDataService from "@/services/sportsDataService";
+import { useQuery } from "@tanstack/react-query";
+import { Loader } from "lucide-react";
+import type { Stat } from "@/types/Stat";
+import AthleteStatsList from "@/components/AthleteStatsList";
+import CreateAthleteStat from "@/components/CreateAthleteStat";
 
 const AthleteStats = () => {
   const { athleteId } = useParams<{ athleteId: string }>();
 
-  const fetchStats = (id?: string) => {
-    if (id) {
-      console.log(id);
-    }
-  };
+  const {
+    data: stats,
+    isLoading,
+    error,
+  } = useQuery<Stat[]>({
+    queryKey: ["stats"],
+    queryFn: () => sportsDataService.getStats(athleteId!),
+    enabled: !!athleteId,
+  });
 
-  useEffect(() => {
-    fetchStats(athleteId);
-  }, []);
+  if (isLoading) return <Loader className="animate-spin" />;
+  if (error) return <p>{error.message}</p>;
+
   return (
     <div>
       AthleteStats
       <div>{athleteId}</div>
+      <CreateAthleteStat />
+      <AthleteStatsList stats={stats!} />
     </div>
   );
 };
