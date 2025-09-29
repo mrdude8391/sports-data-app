37b6befa1461486da7a1595a49e259352ac9ebab
Bryan_w11 <h.bryan68@yahoo.ca>
2025-09-29 17:07:21 -0400

    added button to view athletes stats on athletes page . button links to athletes id route

--- client/src/pages/Athletes.tsx
+++ client/src/pages/Athletes.tsx
@@ -8,6 +8,8 @@ import { Loader } from "lucide-react";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import CreateAthlete from "@/components/CreateAthlete";
 import DeleteAthlete from "@/components/DeleteAthlete";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 const Athletes = () => {
   // const [athletes, setAthletes] = useState<Athlete[]>([]);
@@ -75,6 +77,9 @@ const Athletes = () => {
         athletes.map((a: Athlete) => (
           <div key={a._id} className="flex flex-row">
             <AthleteCard imageSrc={sampleImage} athlete={a.name} />
+            <Link to={`/athletes/${a._id}/stats`}>
+              <Button>View Stats</Button>
+            </Link>
             <DeleteAthlete id={a._id} />
           </div>
         ))}
