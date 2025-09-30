e6a89bb3584b1941ed3564f9ce2877450a10ab0f
Bryan_w11 <h.bryan68@yahoo.ca>
2025-09-30 17:29:04 -0400

    fixed interface to type. added statform type

--- client/src/types/Stat.ts
+++ client/src/types/Stat.ts
@@ -1,8 +1,44 @@
-export interface Stat {
+export type Stat = {
     _id?:string,
     userId: string,
     athleteId: string,
-    type: string,
-    value: number,
+    attack: {
+      kills: number,
+      errors: number,
+      total: number,
+      percentage: number,
+    },
+    setting: {
+      assists: number,
+      errors: number,
+    },
+    serving: {
+      rating: number,
+      aces: number,
+      errors: number,
+      attempts: number,
+    },
+    passing: {
+      rating: number,
+      errors: number,
+      attempts: number,
+    },
+    defense: {
+      digs: number,
+    },
+    blocking: {
+      total: number,
+      kills: number,
+      solos: number,
+      goodTouches: number,
+      attempts: number,
+      errors: number,
+    },
     recordedAt: Date,
 }
+
+export type StatForm = {
+  [category: string]: {
+    [field: string]: number;
+  };
+};
