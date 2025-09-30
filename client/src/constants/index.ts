590faf795969895ff732fb446ac0c2637cf116a1
Bryan_w11 <h.bryan68@yahoo.ca>
2025-09-30 17:30:47 -0400

    added a constant STAT_FIELDS to consistently index the stat fields. that way the fields are not hard coded into the UI

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -6,3 +6,40 @@ export const NAV_LINKS = [
     { href: '/athletes', key: "Athletes", label: "Athletes"  },
 
 ]
+
+export const STAT_FIELDS = {
+    attack: [
+    { key: "kills", label: "Kills" },
+    { key: "errors", label: "Errors" },
+    { key: "total", label: "Total Attacks" },
+    { key: "percentage", label: "Hitting %" },
+  ],
+  setting: [
+    { key: "assists", label: "Assists" },
+    { key: "errors", label: "Errors" },
+  ],
+  serving: [
+    { key: "rating", label: "Rating" },
+    { key: "aces", label: "Aces" },
+    { key: "errors", label: "Errors" },
+    { key: "attempts", label: "Attempts" },
+  ],
+  passing: [
+    { key: "rating", label: "Rating" },
+    { key: "errors", label: "Errors" },
+    { key: "attempts", label: "Attempts" },
+  ],
+  defense : [
+    { key: 'digs', label: "Digs"}
+  ],
+  blocking : [
+    { key: "total", label: "Total Blocks" },
+    { key: "kills", label: "Kills" },
+    { key: "solos", label: "Solo Blocks" },
+    { key: "goodTouches", label: "Good Touches" },
+    { key: "attempts", label: "Attempts" },
+    { key: "errors", label: "Errors" },
+  ]
+    
+}
+
