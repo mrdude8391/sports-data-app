d3ba3e7ddb1470abe5fd2792a918917e116c6b3d
Bryan_w11 <h.bryan68@yahoo.ca>
2025-10-01 14:43:57 -0400

    offloaded the create athlete UI into a dialog button

--- client/src/components/CreateAthlete.tsx
+++ client/src/components/CreateAthlete.tsx
@@ -5,6 +5,18 @@ import { useState } from "react";
 import * as sportsDataService from "@/services/sportsDataService";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import type { Athlete } from "@/types/Athlete";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Alert, AlertTitle } from "./ui/alert";
+import { AlertCircleIcon } from "lucide-react";
 
 const CreateAthlete = () => {
   const [name, setName] = useState("");
@@ -37,45 +49,80 @@ const CreateAthlete = () => {
   };
   return (
     <div>
-      {error && <div>{error.message}</div>}
-      <form id="athleteForm" onSubmit={handleSubmit}>
-        <Label htmlFor="name">Name</Label>
-        <Input
-          type="text"
-          id="name"
-          value={name}
-          placeholder="Name"
-          required
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-        ></Input>
-        <Label htmlFor="age">age</Label>
-        <Input
-          type="number"
-          id="age"
-          value={age}
-          placeholder="Age"
-          required
-          onChange={(e) => {
-            setAge(e.target.value);
-          }}
-        ></Input>
-        <Label htmlFor="height">height</Label>
-        <Input
-          type="number"
-          id="height"
-          value={height}
-          placeholder="Height"
-          required
-          onChange={(e) => {
-            setHeight(e.target.value);
-          }}
-        ></Input>
-      </form>
-      <Button type="submit" form="athleteForm">
-        Create Athlete
-      </Button>
+      <Dialog>
+        <form id="athleteForm" onSubmit={handleSubmit}>
+          <DialogTrigger asChild>
+            <Button variant="outline">Add New Athlete</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Create Athlete</DialogTitle>
+              <DialogDescription>
+                Create and add a new athlete here. Click save when you&apos;re
+                done.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4">
+              <div className="grid gap-3">
+                <Label htmlFor="name">Name</Label>
+                <Input
+                  type="text"
+                  id="name"
+                  value={name}
+                  placeholder="Name"
+                  required
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                ></Input>
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="age">Age</Label>
+                <Input
+                  type="number"
+                  id="age"
+                  value={age}
+                  placeholder="Age"
+                  required
+                  onChange={(e) => {
+                    setAge(e.target.value);
+                  }}
+                ></Input>
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="height">Height</Label>
+                <Input
+                  type="number"
+                  id="height"
+                  value={height}
+                  placeholder="Height"
+                  required
+                  onChange={(e) => {
+                    setHeight(e.target.value);
+                  }}
+                ></Input>
+              </div>
+            </div>
+            {error && (
+              <Alert
+                variant="destructive"
+                className="flex justify-between items-center"
+              >
+                <AlertCircleIcon />
+                <AlertTitle className="text-center">{error.message}</AlertTitle>
+              </Alert>
+            )}
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DialogClose>
+              <Button type="submit" form="athleteForm">
+                Save New Athlete
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </form>
+      </Dialog>
     </div>
   );
 };
