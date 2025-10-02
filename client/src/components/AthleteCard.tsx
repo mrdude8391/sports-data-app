155460f7f4c642d203dc532fa4119cb7628d76a8
Bryan_w11 <h.bryan68@yahoo.ca>
2025-10-02 19:51:37 -0400

    refactored the athlete card to have the view stats and delete button

--- client/src/components/AthleteCard.tsx
+++ client/src/components/AthleteCard.tsx
@@ -1,22 +1,34 @@
 import { Image } from "lucide-react";
 import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+import DeleteAthlete from "./DeleteAthlete";
+import type { Athlete } from "@/types/Athlete";
 
 interface AthleteCardProps {
   imageSrc: string;
-  athlete: string;
+  athlete: Athlete;
 }
 
 const AthleteCard = (props: AthleteCardProps) => {
   const { imageSrc, athlete } = props;
   return (
-    <div className="flex w-full gap-5 items-center border-1 rounded-lg px-5 py-2">
-      <img
-        src={imageSrc}
-        height={48}
-        width={48}
-        className="border-1 rounded-full p-2"
-      />
-      <div className="px-3 py-1 border-1 rounded-lg">{athlete}</div>
+    <div className="flex flex-col gap-2 w-full items-center border-1 rounded-lg px-5 py-2">
+      <div className="flex w-full items-center justify-start rounded-lg px-5 py-2">
+        <img
+          src={imageSrc}
+          height={48}
+          width={48}
+          className="border-1 rounded-full flex-shrink-0"
+        />
+        <div className="px-3 py-1 rounded-lg ">{athlete.name}</div>
+      </div>
+      <div className="flex w-full justify-between">
+        <Link to={`/athletes/${athlete._id}/stats`}>
+          <Button>View Stats</Button>
+        </Link>
+        <DeleteAthlete id={athlete._id} />
+      </div>
     </div>
   );
 };
