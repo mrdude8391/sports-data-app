e79040c2773dea017eaf15f09200c2b501f9eb2b
Bryan_w11 <h.bryan68@yahoo.ca>
2025-10-03 15:49:08 -0400

    added option to delete

--- client/src/pages/Athletes.tsx
+++ client/src/pages/Athletes.tsx
@@ -2,10 +2,14 @@ import AthleteCard from "@/components/AthleteCard";
 import type { Athlete } from "@/types/Athlete";
 import * as sportsDataService from "../services/sportsDataService";
 import sampleImage from "../assets/circle-user-round.svg";
-import { Loader } from "lucide-react";
+import { EllipsisVertical, Loader } from "lucide-react";
 import { useQuery } from "@tanstack/react-query";
 import CreateAthlete from "@/components/CreateAthlete";
+import DeleteAthlete from "@/components/DeleteAthlete";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 const Athletes = () => {
+  const [isEdit, setIsEdit] = useState(false);
   const {
     data: athletes,
     isLoading,
@@ -19,12 +23,25 @@ const Athletes = () => {
   if (error) return <p>... No Athletes Found</p>;
 
   return (
-    <div className="flex flex-col gap-5">
-      <CreateAthlete></CreateAthlete>
+    <div className="sm:w-lg flex flex-col gap-5">
+      <div className="flex justify-between">
+        <CreateAthlete />
+        <Button variant="outline" onClick={() => setIsEdit(!isEdit)}>
+          <EllipsisVertical />
+        </Button>
+      </div>
+
       {athletes && athletes.length > 0 ? (
         athletes.map((a: Athlete) => (
-          <div key={a._id} className="flex flex-row gap-4 items-center">
-            <AthleteCard imageSrc={sampleImage} athlete={a} />
+          <div key={a._id} className="flex w-full flex-row gap-4 items-center">
+            <div className="w-full ">
+              <AthleteCard imageSrc={sampleImage} athlete={a} />{" "}
+            </div>
+            {isEdit && (
+              <div>
+                <DeleteAthlete id={a._id} />
+              </div>
+            )}
           </div>
         ))
       ) : (
