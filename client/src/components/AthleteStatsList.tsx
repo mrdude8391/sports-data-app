d65c7a45bbdf7ed877f73cba81bbdcafd7ea0c48
Bryan_w11 <h.bryan68@yahoo.ca>
2025-10-10 12:32:08 -0400

    removed commented out

--- client/src/components/AthleteStatsList.tsx
+++ client/src/components/AthleteStatsList.tsx
@@ -18,40 +18,6 @@ const AthleteStatsList = (props: AthleteStatsListProps) => {
 
   return (
     <div>
-      {/* {stats && stats.length > 0 ? (
-        <ul>
-          {stats?.map((stat, index) => (
-            <li key={stat._id}>
-              <h3 className="font-bold mb-2">
-                Game {index + 1} (
-                {new Date(stat.recordedAt).toLocaleDateString()})
-              </h3>
-              <div className="grid grid-cols-1 sm:grid-cols-2 gap-y-5 gap-x-10">
-                {Object.entries(STAT_FIELDS).map(([category, fields]) => (
-                  <div key={category} className="mb-2">
-                    <h4 className="font-semibold capitalize">{category}</h4>
-                    <div className="grid grid-cols-2 gap-2">
-                      {fields.map(({ key, label }) => (
-                        <div key={key} className="flex justify-between">
-                          <span>{label}</span>
-                          <span>{(stat as any)[category][key]}</span>
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                ))}
-              </div>
-
-              <div>
-                <DeleteAthleteStat statId={stat._id} />
-              </div>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>No Stats</p>
-      )} */}
-
       {stats && stats.length > 0 ? (
         <ul>
           {stats?.map((stat, index) => (
