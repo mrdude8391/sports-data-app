2833a10625df77e76523e4c4fa3164706cdb2adb
Bryan_w11 <h.bryan68@yahoo.ca>
2025-10-13 12:51:23 -0400

    added the radar chart component removed stat console log added styling for responsiveness to stat page

--- client/src/pages/AthleteStats.tsx
+++ client/src/pages/AthleteStats.tsx
@@ -8,6 +8,7 @@ import AthleteStatsList from "@/components/AthleteStatsList";
 import CreateAthleteStat from "@/components/CreateAthleteStat";
 import type { Athlete } from "@/types/Athlete";
 import AthleteStatChart from "@/components/AthleteStatChart";
+import AthleteStatRadial from "@/components/AthleteStatRadar";
 
 const AthleteStats = () => {
   const { athleteId } = useParams<{ athleteId: string }>();
@@ -28,18 +29,17 @@ const AthleteStats = () => {
     enabled: !!athleteId,
   });
 
-  if (stats) console.log(stats);
-
   if (isLoading) return <Loader className="animate-spin" />;
   if (error) return <p>{error.message}</p>;
 
   return (
-    <div>
+    <div className="flex flex-col gap-6 w-full max-w-6xl py-3">
       <div>
         <p>{cachedAthlete?.name}</p>
         <p>Id {athleteId}</p>
       </div>
       <CreateAthleteStat />
+      <AthleteStatRadial stats={stats!} />
       <AthleteStatChart stats={stats!} />
       <AthleteStatsList stats={stats!} />
     </div>
