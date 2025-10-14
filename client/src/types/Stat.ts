db0556ac0cd3101767d873109d4d62cab3204d35
Bryan_w11 <h.bryan68@yahoo.ca>
2025-10-14 16:55:03 -0400

    added new stats to stat types

--- client/src/types/Stat.ts
+++ client/src/types/Stat.ts
@@ -14,6 +14,7 @@ export type Stat= {
     },
     serving: {
       rating: number,
+      ratingTotal: number,
       aces: number,
       errors: number,
       attempts: number,
@@ -21,12 +22,14 @@ export type Stat= {
     },
     receiving: {
       rating: number,
+      ratingTotal: number,
       errors: number,
       attempts: number,
     },
     defense: {
       digs: number,
       rating: number,
+      ratingTotal: number,
       errors: number,
       attempts: number,
     },
@@ -47,4 +50,181 @@ export type StatForm = {
   [category: string]: {
     [field: string]: number;
   };
+};
+
+export type StatPayload = {
+    attack: {
+      kills: number,
+      errors: number,
+      total: number,
+      percentage: number,
+    },
+    setting: {
+      assists: number,
+      errors: number,
+    },
+    serving: {
+      rating: number,
+      ratingTotal: number,
+      aces: number,
+      errors: number,
+      attempts: number,
+      percentage: number, 
+    },
+    receiving: {
+      rating: number,
+      ratingTotal: number,
+      errors: number,
+      attempts: number,
+    },
+    defense: {
+      digs: number,
+      rating: number,
+      ratingTotal: number,
+      errors: number,
+      attempts: number,
+    },
+    blocking: {
+      total: number,
+      kills: number,
+      solos: number,
+      goodTouches: number,
+      attempts: number,
+      errors: number,
+    },
+    recordedAt: Date,
+}
+
+export type StatResponse= {
+    _id:string,
+    userId: string,
+    athleteId: string,
+    attack: {
+      kills: number,
+      errors: number,
+      total: number,
+      percentage: number,
+    },
+    setting: {
+      assists: number,
+      errors: number,
+    },
+    serving: {
+      rating: number,
+      ratingTotal: number,
+      aces: number,
+      errors: number,
+      attempts: number,
+      percentage: number, 
+    },
+    receiving: {
+      rating: number,
+      ratingTotal: number,
+      errors: number,
+      attempts: number,
+    },
+    defense: {
+      digs: number,
+      rating: number,
+      ratingTotal: number,
+      errors: number,
+      attempts: number,
+    },
+    blocking: {
+      total: number,
+      kills: number,
+      solos: number,
+      goodTouches: number,
+      attempts: number,
+      errors: number,
+    },
+    recordedAt: Date,
+    createdAt: Date,
+    updatedAt: Date
+}
+
+export const InitialStatForm: StatForm = {
+  attack: {
+      kills: 0,
+      errors: 0,
+      total: 0,
+      percentage: 0,
+    },
+    setting: {
+      assists: 0,
+      errors: 0,
+    },
+    serving: {
+      rating: 0,
+      ratingTotal: 0,
+      aces: 0,
+      errors: 0,
+      attempts: 0,
+      percentage: 0, 
+    },
+    receiving: {
+      rating: 0,
+      ratingTotal: 0,
+      errors: 0,
+      attempts: 0,
+    },
+    defense: {
+      digs: 0,
+      rating: 0,
+      ratingTotal: 0,
+      errors: 0,
+      attempts: 0,
+    },
+    blocking: {
+      total: 0,
+      kills: 0,
+      solos: 0,
+      goodTouches: 0,
+      attempts: 0,
+      errors: 0,
+    },
+  
+};
+
+export const InitialStatPayload: StatPayload = {
+  attack: {
+      kills: 0,
+      errors: 0,
+      total: 0,
+      percentage: 0,
+    },
+    setting: {
+      assists: 0,
+      errors: 0,
+    },
+    serving: {
+      rating: 0,
+      ratingTotal: 0,
+      aces: 0,
+      errors: 0,
+      attempts: 0,
+      percentage: 0, 
+    },
+    receiving: {
+      rating: 0,
+      ratingTotal: 0,
+      errors: 0,
+      attempts: 0,
+    },
+    defense: {
+      digs: 0,
+      rating: 0,
+      ratingTotal: 0,
+      errors: 0,
+      attempts: 0,
+    },
+    blocking: {
+      total: 0,
+      kills: 0,
+      solos: 0,
+      goodTouches: 0,
+      attempts: 0,
+      errors: 0,
+    },
+  recordedAt: new Date()
 };
