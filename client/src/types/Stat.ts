0fc3d98f611f3fb607cc09d627e38c1b277288db
Bryan_w11 <h.bryan68@yahoo.ca>
2025-10-15 10:30:50 -0400

    added some stat types that i may use

--- client/src/types/Stat.ts
+++ client/src/types/Stat.ts
@@ -53,6 +53,8 @@ export type StatForm = {
 };
 
 
+
+
 export type StatPayload = {
     attack: {
       kills: number,
@@ -189,45 +191,86 @@ export const InitialStatForm: StatForm = {
   
 };
 
-export const InitialStatPayload: StatPayload = {
-  attack: {
-      kills: 0,
-      errors: 0,
-      total: 0,
-      percentage: 0,
-    },
-    setting: {
-      assists: 0,
-      errors: 0,
-    },
-    serving: {
-      rating: 0,
-      ratingTotal: 0,
-      aces: 0,
-      errors: 0,
-      attempts: 0,
-      percentage: 0, 
-    },
-    receiving: {
-      rating: 0,
-      ratingTotal: 0,
-      errors: 0,
-      attempts: 0,
-    },
-    defense: {
-      digs: 0,
-      rating: 0,
-      ratingTotal: 0,
-      errors: 0,
-      attempts: 0,
-    },
-    blocking: {
-      total: 0,
-      kills: 0,
-      solos: 0,
-      goodTouches: 0,
-      attempts: 0,
-      errors: 0,
-    },
-  recordedAt: new Date()
-};
+// export const InitialStatPayload: StatPayload = {
+//   attack: {
+//       kills: 0,
+//       errors: 0,
+//       total: 0,
+//       percentage: 0,
+//     },
+//     setting: {
+//       assists: 0,
+//       errors: 0,
+//     },
+//     serving: {
+//       rating: 0,
+//       ratingTotal: 0,
+//       aces: 0,
+//       errors: 0,
+//       attempts: 0,
+//       percentage: 0, 
+//     },
+//     receiving: {
+//       rating: 0,
+//       ratingTotal: 0,
+//       errors: 0,
+//       attempts: 0,
+//     },
+//     defense: {
+//       digs: 0,
+//       rating: 0,
+//       ratingTotal: 0,
+//       errors: 0,
+//       attempts: 0,
+//     },
+//     blocking: {
+//       total: 0,
+//       kills: 0,
+//       solos: 0,
+//       goodTouches: 0,
+//       attempts: 0,
+//       errors: 0,
+//     },
+//   recordedAt: new Date()
+// };
+
+// type AttackStat = {
+//       kills: number,
+//       errors: number,
+//       total: number,
+//       percentage: number,
+//     }
+
+// type SettingStat ={
+//       assists: number,
+//       errors: number,
+//     }
+// type ServingStat ={
+//       rating: number,
+//       ratingTotal: number,
+//       aces: number,
+//       errors: number,
+//       attempts: number,
+//       percentage: number, 
+//     }
+// type ReceivingStat = {
+//       rating: number,
+//       ratingTotal: number,
+//       errors: number,
+//       attempts: number,
+//     }
+// type DefenseStat ={
+//       digs: number,
+//       rating: number,
+//       ratingTotal: number,
+//       errors: number,
+//       attempts: number,
+//     }
+// type BlockingStat = {
+//       total: number,
+//       kills: number,
+//       solos: number,
+//       goodTouches: number,
+//       attempts: number,
+//       errors: number,
+//     }
