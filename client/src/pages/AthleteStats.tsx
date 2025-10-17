667f269118dac9969bcb622938d1a66f59e1da03
Bryan_w11 <h.bryan68@yahoo.ca>
2025-10-16 21:07:37 -0400

    added date range selector. added a filtered stats list that populates based on date range selected.

--- client/src/pages/AthleteStats.tsx
+++ client/src/pages/AthleteStats.tsx
@@ -2,16 +2,28 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import * as sportsDataService from "@/services/sportsDataService";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { Loader } from "lucide-react";
+import { ChevronDownIcon, Loader } from "lucide-react";
 import type { Stat } from "@/types/Stat";
 import AthleteStatsList from "@/components/AthleteStatsList";
 import CreateAthleteStat from "@/components/CreateAthleteStat";
 import type { Athlete } from "@/types/Athlete";
 import AthleteStatChart from "@/components/AthleteStatChart";
 import AthleteStatRadial from "@/components/AthleteStatRadar";
+import AthleteStatTable from "@/components/AthleteStatTable";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import type { DateRange } from "react-day-picker";
 
 const AthleteStats = () => {
   const { athleteId } = useParams<{ athleteId: string }>();
+
+  const [open, setOpen] = useState(false);
+  const [date, setDate] = useState<DateRange | undefined>();
 
   const queryClient = useQueryClient();
 
@@ -29,19 +41,76 @@ const AthleteStats = () => {
     enabled: !!athleteId,
   });
 
+  const filteredStats = stats?.filter((stat) => {
+    if (date && date.from && date.to) {
+      const recordedAt = new Date(stat.recordedAt);
+      return recordedAt >= date?.from && recordedAt <= date?.to;
+    }
+  });
+
   if (isLoading) return <Loader className="animate-spin" />;
   if (error) return <p>{error.message}</p>;
 
   return (
     <div className="flex flex-col gap-6 w-full max-w-6xl py-3">
       <div>
-        <p>{cachedAthlete?.name}</p>
+        <h1>{cachedAthlete?.name}</h1>
         <p>Id {athleteId}</p>
       </div>
+      <div>
+        <p>
+          Selected Date{" "}
+          {date && date.from && date.to
+            ? `${date.from.toLocaleDateString()}  ${date.to.toLocaleDateString()}`
+            : ""}
+        </p>
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              id="date"
+              className="w-48 justify-between font-normal"
+            >
+              {date && date.from && date.to
+                ? `${date.from.toLocaleDateString()}  ${date.to.toLocaleDateString()}`
+                : "Select date"}
+              <ChevronDownIcon />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto overflow-hidden p-0" align="start">
+            <Calendar
+              mode="range"
+              selected={date}
+              captionLayout="dropdown"
+              onSelect={(date) => {
+                setDate(date);
+              }}
+            />
+          </PopoverContent>
+        </Popover>
+      </div>
+
       <CreateAthleteStat />
-      <AthleteStatRadial stats={stats!} />
-      <AthleteStatChart stats={stats!} />
-      <AthleteStatsList stats={stats!} />
+      <AthleteStatRadial
+        stats={
+          filteredStats && filteredStats.length > 0 ? filteredStats : stats!
+        }
+      />
+      <AthleteStatChart
+        stats={
+          filteredStats && filteredStats.length > 0 ? filteredStats : stats!
+        }
+      />
+      <AthleteStatTable
+        stats={
+          filteredStats && filteredStats.length > 0 ? filteredStats : stats!
+        }
+      />
+      <AthleteStatsList
+        stats={
+          filteredStats && filteredStats.length > 0 ? filteredStats : stats!
+        }
+      />
     </div>
   );
 };
