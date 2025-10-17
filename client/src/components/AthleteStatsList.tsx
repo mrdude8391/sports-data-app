a6f6b97d43ae8298714235cff7197aff7413827f
Bryan_w11 <h.bryan68@yahoo.ca>
2025-10-16 21:08:31 -0400

    moved that stats table to its own component

--- client/src/components/AthleteStatsList.tsx
+++ client/src/components/AthleteStatsList.tsx
@@ -1,24 +1,5 @@
-import { STAT_FIELDS, STAT_INDEX } from "@/constants";
+import { STAT_INDEX } from "@/constants";
 import type { Stat } from "@/types/Stat";
-import { Button } from "./ui/button";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 
 import {
   Accordion,
@@ -27,9 +8,7 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 
-import * as sportsDataService from "@/services/sportsDataService";
 import DeleteAthleteStat from "./DeleteAthleteStat";
-import { useState } from "react";
 import EditAthleteStat from "./EditAthleteStat";
 
 interface AthleteStatsListProps {
@@ -39,72 +18,8 @@ interface AthleteStatsListProps {
 const AthleteStatsList = (props: AthleteStatsListProps) => {
   const { stats } = props;
 
-  const [data, setData] = useState([]);
-  const [select, setSelect] = useState("");
-
-  const handleValueChange = (value: string) => {
-    const selection: any = STAT_INDEX.find((s) => s.category === value)?.fields;
-    setData(selection);
-    setSelect(value);
-    // Perform any other actions with the selected value here
-  };
-
   return (
     <div className="flex flex-col gap-6">
-      <Select onValueChange={handleValueChange}>
-        <SelectTrigger className="w-full text-2xl font-semibold py-6">
-          <SelectValue
-            className=""
-            placeholder="Select a category to display chart"
-          />
-        </SelectTrigger>
-        <SelectContent>
-          {STAT_INDEX.map(({ category, fields }) => (
-            <SelectGroup key={category}>
-              <SelectItem
-                key={category}
-                value={category}
-                className="capitalize"
-              >
-                {category}
-              </SelectItem>
-            </SelectGroup>
-          ))}
-        </SelectContent>
-      </Select>
-      <Table className="min-w-full divide-y divide-gray-200 text-sm">
-        {select == "" ? (
-          <TableCaption>Select a category to display chart</TableCaption>
-        ) : (
-          <TableCaption>List of stats</TableCaption>
-        )}
-
-        {select !== "" && (
-          <TableHeader>
-            <TableRow>
-              <TableHead>Date</TableHead>
-              {data.map(({ key, label }) => (
-                <TableHead>{label}</TableHead>
-              ))}
-            </TableRow>
-          </TableHeader>
-        )}
-        {select !== "" && (
-          <TableBody>
-            {stats?.map((stat) => (
-              <TableRow key={stat._id}>
-                <TableCell>
-                  {stat.recordedAt.toLocaleDateString("en-UB")}
-                </TableCell>
-                {data.map(({ key, label }) => (
-                  <TableCell>{(stat as any)[select][key]}</TableCell>
-                ))}
-              </TableRow>
-            ))}
-          </TableBody>
-        )}
-      </Table>
-
       {stats.length > 0 ? (
         <Accordion type="single" collapsible className="w-full">
           {stats.map((stat, index) => (
