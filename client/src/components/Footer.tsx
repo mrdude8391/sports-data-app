5e61ea602878293ba132df14f8628af08fbf4113
Bryan_w11 <h.bryan68@yahoo.ca>
2025-10-16 23:11:58 -0400

    Added real words to the footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="flex w-full justify-center bg-white">
-      <div className="max-container padding-container">Footer</div>
+    <footer className="flex w-full flex-col justify-center bg-white ">
+      <div className="max-container padding-container m-3 ">
+        Volleyball Stat App
+      </div>
     </footer>
   );
 };
