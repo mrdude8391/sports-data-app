145c5c3994ecbaf6750cdb9baa0fa1c73a29dc57
Bryan_w11 <h.bryan68@yahoo.ca>
2025-10-16 21:08:15 -0400

    moved stat table to its own component

---
+++ client/src/components/AthleteStatTable.tsx
@@ -0,0 +1,101 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useState } from "react";
+import { STAT_INDEX } from "@/constants";
+import type { Stat } from "@/types/Stat";
+
+interface AthleteStatTableProps {
+  stats: Stat[];
+}
+
+const AthleteStatTable = (props: AthleteStatTableProps) => {
+  const { stats } = props;
+  const [data, setData] = useState([]);
+  const [select, setSelect] = useState("");
+
+  const capitalize = (str: string) => {
+    if (str == undefined || str.length === 0) return "";
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+
+  const handleValueChange = (value: string) => {
+    const selection: any = STAT_INDEX.find((s) => s.category === value)?.fields;
+    setData(selection);
+    setSelect(value);
+    // Perform any other actions with the selected value here
+  };
+  return (
+    <div className="flex flex-col gap-6">
+      <Select onValueChange={handleValueChange}>
+        <SelectTrigger className="w-full text-2xl font-semibold py-6">
+          <SelectValue
+            className=""
+            placeholder="Select a category to display chart"
+          />
+        </SelectTrigger>
+        <SelectContent>
+          {STAT_INDEX.map(({ category, fields }) => (
+            <SelectGroup key={category}>
+              <SelectItem
+                key={category}
+                value={category}
+                className="capitalize"
+              >
+                {capitalize(category)}
+              </SelectItem>
+            </SelectGroup>
+          ))}
+        </SelectContent>
+      </Select>
+      <Table className="min-w-full divide-y divide-gray-200 text-sm">
+        {select == "" ? (
+          <TableCaption>Select a category to display chart</TableCaption>
+        ) : (
+          <TableCaption>Stat table by category</TableCaption>
+        )}
+
+        {select !== "" && (
+          <TableHeader>
+            <TableRow>
+              <TableHead>Date</TableHead>
+              {data.map(({ key, label }) => (
+                <TableHead>{label}</TableHead>
+              ))}
+            </TableRow>
+          </TableHeader>
+        )}
+        {select !== "" && (
+          <TableBody>
+            {stats?.map((stat) => (
+              <TableRow key={stat._id}>
+                <TableCell>
+                  {stat.recordedAt.toLocaleDateString("en-UB")}
+                </TableCell>
+                {data.map(({ key, label }) => (
+                  <TableCell>{(stat as any)[select][key]}</TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        )}
+      </Table>
+    </div>
+  );
+};
+
+export default AthleteStatTable;
