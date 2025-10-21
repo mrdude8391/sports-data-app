537124f6bc08d1b033cbe1e746f5fcf310307ae5
Bryan_w11 <h.bryan68@yahoo.ca>
2025-10-21 16:44:58 -0400

    athletes page divs reformatted

--- client/src/pages/Athletes.tsx
+++ client/src/pages/Athletes.tsx
@@ -5,9 +5,9 @@ import sampleImage from "../assets/circle-user-round.svg";
 import { EllipsisVertical, Loader } from "lucide-react";
 import { useQuery } from "@tanstack/react-query";
 import CreateAthlete from "@/components/CreateAthlete";
-import DeleteAthlete from "@/components/DeleteAthlete";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
+
 const Athletes = () => {
   const [isEdit, setIsEdit] = useState(false);
   const {
@@ -23,7 +23,7 @@ const Athletes = () => {
   if (error) return <p>... No Athletes Found</p>;
 
   return (
-    <div className="sm:w-lg flex flex-col gap-5">
+    <div className="sm:w-lg flex flex-col gap-6">
       <div className="flex justify-between">
         <CreateAthlete />
         <Button variant="outline" onClick={() => setIsEdit(!isEdit)}>
@@ -31,17 +31,26 @@ const Athletes = () => {
         </Button>
       </div>
 
-      {athletes && athletes.length > 0 ? (
-        athletes.map((a: Athlete) => (
-          <div key={a._id} className="flex w-full flex-row gap-4 items-center">
-            <div className="w-full ">
-              <AthleteCard imageSrc={sampleImage} athlete={a} isEdit={isEdit} />{" "}
+      <div className="flex flex-col gap-3">
+        {athletes && athletes.length > 0 ? (
+          athletes.map((a: Athlete) => (
+            <div
+              key={a._id}
+              className="flex w-full flex-row gap-4 items-center"
+            >
+              <div className="w-full ">
+                <AthleteCard
+                  imageSrc={sampleImage}
+                  athlete={a}
+                  isEdit={isEdit}
+                />{" "}
+              </div>
             </div>
-          </div>
-        ))
-      ) : (
-        <p>No Athletes</p>
-      )}
+          ))
+        ) : (
+          <p>No Athletes</p>
+        )}
+      </div>
     </div>
   );
 };
