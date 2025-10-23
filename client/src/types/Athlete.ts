0ec3021f10fe93a0eb6ab86c9739925f804af4f8
Bryan_w11 <h.bryan68@yahoo.ca>
2025-10-23 12:16:31 -0400

    distinct Athlete type and payload for creating athlete

--- client/src/types/Athlete.ts
+++ client/src/types/Athlete.ts
@@ -1,5 +1,11 @@
 export type Athlete = {
-    _id?:string,
+    _id:string,
+    name: string,
+    age: number,
+    height: number,
+}
+
+export type AthletePayload = {
     name: string,
     age: number,
     height: number,
