efbd3132fd25e9eed10e0630ef2d87f7d287d578
Bryan_w11 <h.bryan68@yahoo.ca>
2025-10-23 13:32:36 -0400

    deleted user type and replaced  with centralized auth type file

--- client/src/types/Auth.ts
+++ client/src/types/Auth.ts
@@ -1,13 +1,17 @@
-export type UserPayload = {
+export type LoginPayload = {
+    email: string;
+    password: string;
+}
+
+export type RegisterPayload = {
     username: string,
     email: string,
     password: string
 }
 
-export type UserResponse = {
+export type AuthResponse = {
     _id: string,
     username: string,
     email: string,
     token: string
 }
-
