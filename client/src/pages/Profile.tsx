a68d9878343babac9f623f0186e045ea79d8e0dd
Bryan_w11 <h.bryan68@yahoo.ca>
2025-10-24 13:26:43 -0400

    profile page icon background white remove unsused decs

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,8 +1,6 @@
-import AthleteCard from "@/components/AthleteCard";
 import sampleImage from "../assets/circle-user-round.svg";
 import { useEffect, useState } from "react";
 import { useAuth } from "@/context/AuthContext";
-import * as sportsDataService from "../services/sportsDataService";
 import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
@@ -16,7 +14,12 @@ const Profile = () => {
   return (
     <div className="flex flex-col gap-2">
       <div className="flex gap-5 items-center  rounded-lg px-5 py-2">
-        <img height={48} width={48} src={sampleImage}></img>
+        <img
+          height={48}
+          width={48}
+          src={sampleImage}
+          className="border-1 rounded-full bg-white"
+        ></img>
         <div className="px-3 py-1 border-1 rounded-lg">{user?.username}</div>
       </div>
       <div className="flex flex-col gap-5 py-10 px-15 border-2 rounded-xl">
