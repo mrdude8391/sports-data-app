a97597bdbd947c80e7be55eb58c0e137ac6865ac
Bryan_w11 <h.bryan68@yahoo.ca>
2025-10-23 21:35:58 -0400

    added receiving stat explanation to about page

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -9,6 +9,10 @@ const About = () => {
         An app to record stats for your volleyball players and visualize your
         progress
       </h1>
+      <h2>Radar chart receiving rating</h2>
+      <p>1 -&gt; 0%</p>
+      <p>2 -&gt; 50%</p>
+      <p>3 -&gt; 100%</p>
     </div>
   );
 };
