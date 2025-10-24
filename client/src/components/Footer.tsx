2324feb50f52bf61f851e7ec610109eb43d8ce6d
Bryan_w11 <h.bryan68@yahoo.ca>
2025-10-24 13:25:20 -0400

    footer baground depend on theme

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="flex w-full flex-col justify-center bg-white ">
+    <footer className="flex w-full flex-col justify-center ">
       <div className="max-container padding-container m-3 ">
         Volleyball Stat App
       </div>
