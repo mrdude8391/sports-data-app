24a4ccfcfaa8d0ac3f56ec4ab25e12710f036fff
Bryan_w11 <h.bryan68@yahoo.ca>
2025-10-28 16:45:37 -0400

    added y margin to main child components

--- client/src/pages/layouts/MainLayout.tsx
+++ client/src/pages/layouts/MainLayout.tsx
@@ -6,7 +6,7 @@ const MainLayout = () => {
   return (
     <>
       <Navbar />
-      <div className="flex flex-col mx-auto padding-container w-full max-w-screen min-h-svh items-center">
+      <div className="flex flex-col mx-auto my-4 padding-container w-full max-w-screen min-h-svh items-center">
         <Outlet />
       </div>
       <Footer />
