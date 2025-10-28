6fb474e26c2be7eb141b2d8afd5cde2882524f71
Bryan_w11 <h.bryan68@yahoo.ca>
2025-10-28 13:10:11 -0400

    added framework for different home pages based on login status

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { useAuth } from "@/context/AuthContext";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { isLoggedIn } = useAuth();
+  return (
+    <div>
+      {isLoggedIn ? (
+        <div>Logged in home</div>
+      ) : (
+        <div className="flex flex-col gap-3">
+          <p>! Logged in Home</p>
+          <Link key="Login" to="/login">
+            <Button>Click to login</Button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Home;
