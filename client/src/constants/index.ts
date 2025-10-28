8e316c7706c35e15a028e37679e6918b09a6a522
Bryan_w11 <h.bryan68@yahoo.ca>
2025-10-28 13:09:52 -0400

    removed login from nav links

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,10 +1,9 @@
 export const NAV_LINKS = [
     { href: '/', key: "Home", label: "Home"  },
     { href: '/about', key: "About", label: "About"  },
-    { href: '/login', key: "Login", label: "Login"  },
+    // { href: '/login', key: "Login", label: "Login"  },
     { href: '/profile', key: "Profile", label: "Profile"  },
     { href: '/athletes', key: "Athletes", label: "Athletes"  },
-
 ]
 
 export const STAT_FIELDS = {
