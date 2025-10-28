5cbdec0b8194e8190e5388807cce8a5c38fd927e
Bryan_w11 <h.bryan68@yahoo.ca>
2025-10-28 16:45:19 -0400

    profile page use card container and more uniform styling

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -13,16 +13,16 @@ const Profile = () => {
 
   return (
     <div className="flex flex-col gap-2">
-      <div className="flex gap-5 items-center  rounded-lg px-5 py-2">
+      <div className="flex gap-5 items-center rounded-lg px-5 py-2">
         <img
           height={48}
           width={48}
           src={sampleImage}
-          className="border-1 rounded-full bg-white"
+          className="border shadow-xs rounded-full bg-white"
         ></img>
-        <div className="px-3 py-1 border-1 rounded-lg">{user?.username}</div>
+        <div className="px-3 py-1 card-container">{user?.username}</div>
       </div>
-      <div className="flex flex-col gap-5 py-10 px-15 border-2 rounded-xl">
+      <div className="flex flex-col card-container items-center">
         <p>Profile Stuff</p>
       </div>
     </div>
