46cac5a5706768a5195acc4b5509c3fc3b664fc5
Bryan_w11 <h.bryan68@yahoo.ca>
2025-10-31 15:03:51 -0400

    added pagination

--- client/src/components/AthleteStatTable.tsx
+++ client/src/components/AthleteStatTable.tsx
@@ -15,6 +15,16 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 import { useState } from "react";
 import { STAT_INDEX } from "@/constants";
 import type { Stat } from "@/types/Stat";
@@ -25,8 +35,15 @@ interface AthleteStatTableProps {
 
 const AthleteStatTable = (props: AthleteStatTableProps) => {
   const { stats } = props;
-  const [data, setData] = useState([]);
+  const [selectedStatCategory, setSelectedStatCategory] = useState([]);
   const [select, setSelect] = useState("");
+
+  const [page, setPage] = useState(0);
+  const [count, setCount] = useState(10);
+
+  const firstStatIdx = page * count;
+  const lastStatIdx = firstStatIdx + count;
+  const currentStats = stats.slice(firstStatIdx, lastStatIdx);
 
   const capitalize = (str: string) => {
     if (str == undefined || str.length === 0) return "";
@@ -35,7 +52,7 @@ const AthleteStatTable = (props: AthleteStatTableProps) => {
 
   const handleValueChange = (value: string) => {
     const selection: any = STAT_INDEX.find((s) => s.category === value)?.fields;
-    setData(selection);
+    setSelectedStatCategory(selection);
     setSelect(value);
     // Perform any other actions with the selected value here
   };
@@ -64,7 +81,7 @@ const AthleteStatTable = (props: AthleteStatTableProps) => {
       </Select>
       <Table className="min-w-full divide-y divide-gray-200 text-sm">
         {select == "" ? (
-          <TableCaption>Select a category to display chart</TableCaption>
+          <TableCaption>Select a category to display table</TableCaption>
         ) : (
           <TableCaption>Stat table by category</TableCaption>
         )}
@@ -73,7 +90,7 @@ const AthleteStatTable = (props: AthleteStatTableProps) => {
           <TableHeader>
             <TableRow>
               <TableHead>Date</TableHead>
-              {data.map(({ key, label }) => (
+              {selectedStatCategory.map(({ key, label }) => (
                 <TableHead key={key}>{label}</TableHead>
               ))}
             </TableRow>
@@ -81,12 +98,12 @@ const AthleteStatTable = (props: AthleteStatTableProps) => {
         )}
         {select !== "" && (
           <TableBody>
-            {stats?.map((stat) => (
+            {currentStats?.map((stat) => (
               <TableRow key={stat._id}>
                 <TableCell>
                   {stat.recordedAt.toLocaleDateString("en-UB")}
                 </TableCell>
-                {data.map(({ key, label }) => (
+                {selectedStatCategory.map(({ key, label }) => (
                   <TableCell key={key}>{(stat as any)[select][key]}</TableCell>
                 ))}
               </TableRow>
@@ -94,6 +111,58 @@ const AthleteStatTable = (props: AthleteStatTableProps) => {
           </TableBody>
         )}
       </Table>
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious
+              className={
+                page === 0 ? "cursor-not-allowed opacity-20" : "cursor-pointer"
+              }
+              onClick={() =>
+                setPage((prev) => {
+                  if (prev > 0) {
+                    return prev - 1;
+                  }
+                  return 0;
+                })
+              }
+            />
+          </PaginationItem>
+          <PaginationItem hidden={lastStatIdx < stats.length}>
+            <PaginationLink>{page - 1}</PaginationLink>
+          </PaginationItem>
+          <PaginationItem hidden={page === 0}>
+            <PaginationLink>{page}</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink isActive={true}>{page + 1}</PaginationLink>
+          </PaginationItem>
+          <PaginationItem hidden={lastStatIdx > stats.length}>
+            <PaginationLink>{page + 2}</PaginationLink>
+          </PaginationItem>
+          <PaginationItem hidden={page !== 0}>
+            <PaginationLink>{page + 3}</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext
+              className={
+                lastStatIdx > stats.length
+                  ? "cursor-not-allowed opacity-20"
+                  : "cursor-pointer"
+              }
+              onClick={() =>
+                setPage((prev) => {
+                  if (lastStatIdx < stats.length) return prev + 1;
+                  return prev;
+                })
+              }
+            />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </div>
   );
 };
