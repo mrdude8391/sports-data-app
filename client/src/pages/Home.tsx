b5da1be84c9dca43bb68fe8f9e5a920d573bbc1f
Bryan_w11 <h.bryan68@yahoo.ca>
2025-11-14 12:31:18 -0500

    home page added login and register buttons to home page when not logged in. when logged in added navitagion to atheltes

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -8,18 +8,26 @@ const Home = () => {
     <div className="flex flex-col w-full h-svh items-center justify-start">
       {isLoggedIn ? (
         <div className="card-container sm:w-2xl flex flex-col gap-4 justify-center">
-          <h1>Welcome back to your volleyball tracker</h1>
+          <h1>Welcome to your volleyball tracker!</h1>
 
           <Link key="Login" to="/athletes">
             <Button variant="secondary">Go to athletes</Button>
           </Link>
         </div>
       ) : (
-        <div className="card-container flex flex-col items-center gap-3">
-          <p>! Logged in Home</p>
-          <Link key="Login" to="/login">
-            <Button>Click to login</Button>
-          </Link>
+        <div className="card-container flex flex-col  gap-4">
+          <h1>Welcome to your volleyball tracker!</h1>
+          <p className="text-foreground/80">
+            You are currently not logged in. Please login or sign up!
+          </p>
+          <div className="flex gap-4">
+            <Link key="Login" to="/login">
+              <Button>Login</Button>
+            </Link>
+            <Link key="Register" to="/register">
+              <Button variant="secondary">Sign Up</Button>
+            </Link>
+          </div>
         </div>
       )}
     </div>
