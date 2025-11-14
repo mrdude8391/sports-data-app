794805974642759ebe543d580a2422aaad750ce7
Bryan_w11 <h.bryan68@yahoo.ca>
2025-11-14 12:30:45 -0500

    update login

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -46,7 +46,7 @@ const Login = () => {
 
   return (
     <>
-      <div className="flex w-full min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full flex-col items-center  gap-6 p-6 md:p-10">
         <div className="flex w-full max-w-sm flex-col gap-6">
           <a
             href="#"
