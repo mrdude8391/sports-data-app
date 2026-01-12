d923ea80bfdb268fdc6b42c84d335a518165ffee
bryan_mac <h.bryan68@yahoo.ca>
2026-01-12 15:09:07 -0500

    moved game log

--- client/src/pages/game-log/GameLog.tsx
+++ client/src/pages/game-log/GameLog.tsx
@@ -1,28 +1,16 @@
 import { DatePicker } from "@/components/shared/DatePicker";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
-import * as sportsDataService from "../services/sportsDataService";
-import { Loader, Volleyball } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+import * as sportsDataService from "../../services/sportsDataService";
 import type { Athlete } from "@/types/Athlete";
 import CreateAthleteStat from "@/components/CreateAthleteStat";
-import CreateAthlete from "@/components/CreateAthlete";
 import {
   initialStatForm,
   type StatCategory,
   type StatFieldKey,
   type StatForm,
 } from "@/types/Stat";
+import AthleteSelector from "./components/AthleteSelector";
 
 const GameLog = () => {
   const [selectedDate, setSelectedDate] = useState<Date>(new Date());
@@ -189,7 +177,7 @@ const GameLog = () => {
           ></DatePicker>
         </div>
         <div>
-          <h3>Athlete Selector</h3>
+          <h3>Athletes Selector</h3>
           <AthleteSelector
             selectedAthletes={selectedAthletes}
             handleSelectAthlete={handleSelectAthlete}
@@ -220,70 +208,3 @@ const GameLog = () => {
 };
 
 export default GameLog;
-
-{
-  /* Athlete Selector */
-}
-
-const AthleteSelector = (athleteSelectorProps: {
-  selectedAthletes: Set<Athlete>;
-  handleSelectAthlete: (athlete: Athlete) => void;
-}) => {
-  const { selectedAthletes, handleSelectAthlete } = athleteSelectorProps;
-  const {
-    data: athletes,
-    isLoading,
-    error,
-  } = useQuery({
-    queryKey: ["athletes"],
-    queryFn: sportsDataService.getAthletes,
-  });
-
-  if (isLoading) return <Loader className="animate-spin" />;
-  if (error) return <p>... No Athletes Found</p>;
-  return (
-    <div>
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button variant="outline">Add players</Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Add Athletes to Game</DialogTitle>
-            <DialogDescription>
-              Select the athletes that played this game that you need to add
-              stats for.
-            </DialogDescription>
-          </DialogHeader>
-          <CreateAthlete></CreateAthlete>
-          <ul className="flex flex-col gap-2">
-            {athletes && athletes.length > 0 ? (
-              athletes.map((athlete) => (
-                <li
-                  key={athlete._id}
-                  className={`px-2 rounded cursor-pointer 
-                      ${
-                        selectedAthletes.has(athlete)
-                          ? "bg-accent hover:bg-accent/80"
-                          : "hover:bg-accent/50"
-                      }
-                    `}
-                  onClick={() => handleSelectAthlete(athlete)}
-                >
-                  <p>{athlete.name}</p>
-                </li>
-              ))
-            ) : (
-              <div>No Athletes Found</div>
-            )}
-          </ul>
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button>Save Changes</Button>
-            </DialogClose>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </div>
-  );
-};
