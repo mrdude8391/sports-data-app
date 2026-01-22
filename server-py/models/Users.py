d6139f8039d9cc2c94443d4f03c2a4d57d00d70b
Bryan_w11 <h.bryan68@yahoo.ca>
2026-01-22 14:52:59 -0500

    update user model use uuid

--- server-py/models/Users.py
+++ server-py/models/Users.py
@@ -1,10 +1,16 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.sql import func
 from db import Base
+import uuid
+
 
 class Users(Base):
-    __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True, index=True)
+    __tablename__ = "users"
+    
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
     username = Column(String, nullable=False)
-    email = Column(String, nullable=False, unique=True)
+    email = Column(String, unique=True, nullable=False, index=True)
     password = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
