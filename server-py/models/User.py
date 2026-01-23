7e9e631098207114c40939d8c37085f26299630c
Bryan_w11 <h.bryan68@yahoo.ca>
2026-01-23 15:17:55 -0500

    added athlete relationship to the User class

--- server-py/models/User.py
+++ server-py/models/User.py
@@ -1,7 +1,8 @@
+import uuid
 from sqlalchemy import Column, String, Integer, DateTime
 from sqlalchemy.dialects.postgresql import UUID
-import uuid
 from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
 from database import Base
 
 
@@ -9,9 +10,15 @@ from database import Base
 class User(Base):
     __tablename__ = "users"
     
+    # Columns in databse
     id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
     username = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False, index=True)
     password = Column(String, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    # Relationships for ORM
+    # The relationship is only able to be seen when developing with the ORM, it doesn't do anything on the database side. That is handeld by the ForeignKey.
+    athletes = relationship("Athlete", back_populates="user")
+    # ORM will allow you to access related objects as if they were standard Python attributes
