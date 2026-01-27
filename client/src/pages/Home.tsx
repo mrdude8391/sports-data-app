016475779ed0a0ff131270ba041bee26d85e495a
Bryan_w11 <h.bryan68@yahoo.ca>
2026-01-27 11:19:33 -0500

    added status check to home test button

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,10 +3,13 @@ import { useAuth } from "@/context/AuthContext";
 import { useTheme } from "@/context/ThemeProvider";
 import { Link } from "react-router-dom";
 import { test } from "../services/ApiTestService";
+import { useState } from "react";
 
 const Home = () => {
   const { isLoggedIn } = useAuth();
   const { theme } = useTheme();
+
+  const [status, setStatus] = useState<number | null>(null);
   return (
     <div className="flex flex-col w-full h-svh items-center justify-start">
       <div className="card-container sm:w-2xl flex flex-col gap-4 justify-center">
@@ -14,13 +17,15 @@ const Home = () => {
 
         <Button
           variant={theme == "dark" ? "secondary" : "default"}
-          onClick={() => {
-            const res = test();
+          onClick={async () => {
+            const res = await test();
             console.log(res);
+            setStatus(res.status);
           }}
         >
           Test
         </Button>
+        {status && <div>{status}</div>}
       </div>
       {isLoggedIn ? (
         <div className="card-container sm:w-2xl flex flex-col gap-4 justify-center">
