050a837091a910da36fb331f0375f2c2fb72cbf3
Bryan_w11 <h.bryan68@yahoo.ca>
2026-01-27 12:34:06 -0500

    updated venv

--- server-py/routers/auth_router.py
+++ server-py/routers/auth_router.py
@@ -9,7 +9,7 @@ router = APIRouter()
 @router.post("/register", response_model=UserWithToken)
 def register(user_data: RegisterPayload, db: Session = Depends(get_db)):
     """Register a new user
-
+    
     Returns a UserWithToken Schema. Allowing client to store token               
     """
     return register_user(user_data, db)
