3e7abee1c53d85721e46ce474017dcc440281a3e
Bryan_w11 <h.bryan68@yahoo.ca>
2026-01-29 16:19:28 -0500

    import models

--- server-py/main.py
+++ server-py/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database import Base, engine
 from routers import auth_router, athlete_router
+import models
 
 app = FastAPI()
 
