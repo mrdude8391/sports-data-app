1c65f0508404b285efec8e3c5bdd949363226c58
Bryan_w11 <h.bryan68@yahoo.ca>
2026-01-29 16:24:37 -0500

    create new athlete logic

--- server-py/controllers/athlete_controller.py
+++ server-py/controllers/athlete_controller.py
@@ -1,7 +1,32 @@
 from fastapi import HTTPException
+from sqlalchemy.orm import Session 
+from schemas.athlete_schemas import AthleteCreate, AthleteResponse
+from models import Athlete, User
 
-def create_athlete():
+def create_athlete(athlete_info: AthleteCreate, db: Session, current_user: User) -> AthleteResponse:
+    """Create a new athlete in the database
+
+    Returns an athlete object
+    """
     try:
-        return
+        # Check database for existing athlete name
+        existing_user = db.query(Athlete).filter(Athlete.name == athlete_info.name).first()
+        if existing_user:
+            raise HTTPException(status_code=400, detail="Athlete name already exists")
+        
+        # Create new athlete object
+        new_athlete = Athlete(
+            user_id=current_user.id,
+            name=athlete_info.name,
+            age=athlete_info.age,
+            height=athlete_info.height
+        )
+        
+        # Insert new athlete
+        db.add(new_athlete)
+        db.commit()
+        db.refresh(new_athlete)
+
+        return AthleteResponse.model_validate(new_athlete)
     except ValueError as err:
         raise HTTPException(status_code=500, detail="Create Athlete Failed")
