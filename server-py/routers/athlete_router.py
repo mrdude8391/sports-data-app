c7ed0c7025e5b15cbff085b1e60c541ce8b1f464
Bryan_w11 <h.bryan68@yahoo.ca>
2026-01-29 17:26:47 -0500

    used proper dependencies in the athlete router

--- server-py/routers/athlete_router.py
+++ server-py/routers/athlete_router.py
@@ -3,14 +3,15 @@ from sqlalchemy.orm import Session
 from database import get_db
 from schemas.athlete_schemas import AthleteCreate, AthleteResponse
 from controllers.athlete_controller import create_athlete
-from dependencies.auth import protect
+from dependencies.auth import get_current_user
+from models import User
 
-router = APIRouter(dependencies=[Depends(protect)])
+router = APIRouter()
 
 @router.post("/create", response_model=AthleteResponse)
-def create(athlete_info: AthleteCreate, db: Session = Depends(get_db)):
+def create(athlete_info: AthleteCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     """Create a new athlete            
     """
     print(athlete_info)
-    return create_athlete()
+    return create_athlete(athlete_info, db, current_user)
 
