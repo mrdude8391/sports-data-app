b43ba8baed8a2aaa1f1beb932ef5ac9a0824df44
Bryan_w11 <h.bryan68@yahoo.ca>
2026-02-02 16:41:16 -0500

    create get and delete athlete. handle errors and logging

--- server-py/controllers/athlete_controller.py
+++ server-py/controllers/athlete_controller.py
@@ -4,15 +4,16 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession 
 from schemas.athlete_schemas import AthleteCreate, AthleteResponse
 from models import Athlete, User
+from uuid import UUID
+
 
 async def create_athlete(athlete_info: AthleteCreate, db: AsyncSession, current_user: User) -> AthleteResponse:
-    """Create a new athlete in the database
-
-    Returns an athlete object
-    """
+    """Create a new athlete in the database"""
     try:
+        print("\nLog:\tcreate_athlete() => Create new athlete")
         # Check database for existing athlete name
-        existing_user = await db.query(Athlete).filter(Athlete.name == athlete_info.name).first()
+        results = await db.execute(select(Athlete).filter(Athlete.name == athlete_info.name))
+        existing_user = results.scalars().first()
         if existing_user:
             raise HTTPException(status_code=400, detail="Athlete name already exists")
         
@@ -23,7 +24,6 @@ async def create_athlete(athlete_info: AthleteCreate, db: AsyncSession, current_
             age=athlete_info.age,
             height=athlete_info.height
         )
-        
         # Insert new athlete
         db.add(new_athlete)
         # Add doesnt occur in the db only commit
@@ -31,26 +31,32 @@ async def create_athlete(athlete_info: AthleteCreate, db: AsyncSession, current_
         await db.refresh(new_athlete)
 
         return AthleteResponse.model_validate(new_athlete)
-    except Exception as err:
+    except ValueError as err:
         raise HTTPException(status_code=500, detail="Failed to create new athlete")
     
 async def get_athletes(db: AsyncSession, current_user: User) -> List[AthleteResponse]:
-    """
-    Returns all athletes of current user ID
-    """
+    """Returns all athletes of current user ID"""
     try:
+        print("\nLog:\tget_athletes() => Get all athletes with user ID foreign key")
         # Retreive all athletes whos user_id foreign key === the user id of the requester
         results = await db.execute(select(Athlete).filter(Athlete.user_id == current_user.id))
         athletes = results.scalars().all()
-        print(athletes)
         # Validate and return the response
         return [AthleteResponse.model_validate(athlete) for athlete in athletes]
-    except Exception:
+    except ValueError:
         raise HTTPException(status_code=500, detail=f"Get all athletes Failed")
 
-async def delete_athlete(id, db: AsyncSession, current_user: User):
+async def delete_athlete(id:UUID, db: AsyncSession, current_user: User):
     try:
-        print("delete Athlete")
-    except Exception as err:
+        print("\nLog:\tdelete_athlete() => Delete athlete with ID provided from path parameter")
+        # Retrieve the object
+        result = await db.execute(select(Athlete).where(Athlete.id == id))
+        athlete_to_delete = result.scalar_one_or_none()
+
+        if athlete_to_delete:
+            await db.delete(athlete_to_delete)
+            await db.commit()
+        
+    except ValueError as err:
         raise HTTPException(status_code=500, detail="Create Athlete Failed")
  
