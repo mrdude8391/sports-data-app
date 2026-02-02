73ebf32ddfccbd591683a54f62dd75bce6d39c9f
Bryan_w11 <h.bryan68@yahoo.ca>
2026-02-02 15:54:54 -0500

    async router

--- server-py/routers/auth_router.py
+++ server-py/routers/auth_router.py
@@ -4,20 +4,22 @@ from database import get_db
 from schemas.auth_schemas import LoginPayload, RegisterPayload, UserWithToken
 from controllers.auth_controller import register_user, login_user
 
+# Auth Router
+# Prefix prefix="/auth"
 router = APIRouter()
 
 @router.post("/register", response_model=UserWithToken)
-def register(user_data: RegisterPayload, db: Session = Depends(get_db)):
+async def register(user_data: RegisterPayload, db: Session = Depends(get_db)):
     """Register a new user
 
     Returns a UserWithToken Schema. Allowing client to store token               
     """
-    return register_user(user_data, db)
+    return await register_user(user_data, db)
 
 @router.post("/login", response_model=UserWithToken)
-def login(login_payload: LoginPayload, db: Session = Depends(get_db)):
+async def login(login_payload: LoginPayload, db: Session = Depends(get_db)):
     """Login with provided credentials
 
     Returns a UserWithToken Schema. Allowing client to store token               
     """
-    return login_user(login_payload, db)
+    return await login_user(login_payload, db)
