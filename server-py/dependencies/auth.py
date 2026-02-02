61c6d292747e7667c60ecb8a0b8c86b663f5c1ba
Bryan_w11 <h.bryan68@yahoo.ca>
2026-02-02 15:54:07 -0500

    async and logging

--- server-py/dependencies/auth.py
+++ server-py/dependencies/auth.py
@@ -1,5 +1,7 @@
+import traceback
 from fastapi import Request, HTTPException, Depends, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from sqlalchemy import select
 from database import get_db
 from sqlalchemy.orm import Session
 from models import User
@@ -18,45 +20,45 @@ async def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(s
 
     Returns the current user object after verifying existence in db
     """
+    print("\nLog:\tget_current_user() => Dependency called")
     # Retreive token from credentials dependency
     token = credentials.credentials
-    
-    print("get current user")
     try:
         # Decode the JWT into the payload structures
         # {"id" : str(existing_user.id)}
-        print("start payload")
+        print("\tTry Decode JWT")
         payload = jwt.decode(
             token,
             key=os.getenv("JWT_SECRET"),
             algorithms=["HS256"]
         )
-        
 
         # Extract ID from the payload
         user_id = payload.get("id")
         if not user_id:
-            
             raise HTTPException(status.HTTP_401_UNAUTHORIZED, "Invalid token payload")
-
+        
         # Verify User existence
-        user: User =  db.query(User).filter(User.id == user_id).first()
+        results = await db.execute(select(User).filter(User.id == user_id))
+        user: User =  results.scalars().first()
         if not user:
             raise HTTPException(status.HTTP_404_NOT_FOUND, "User id not found")
-
+        
         # Return User object
         return user
-    except ValueError:
-        print("Payload error")
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Server Error",
-        )
-    except jwt.ExpiredSignatureError:
-        print("expired")
+    except jwt.ExpiredSignatureError as err:
+        print("\tToken expired")
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Expired",
+            detail=f"Expired: {err}"
         )
+    except Exception as err:
+        traceback.print_exc()
+        print(f"\tException: {err}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Server Error : {err}"
+        )
+    
     
     
