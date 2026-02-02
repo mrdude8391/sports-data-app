ea4ea30152fdf32e8a12cb4802924786ff34ad7c
Bryan_w11 <h.bryan68@yahoo.ca>
2026-02-02 15:54:33 -0500

    async router

--- server-py/routers/athlete_router.py
+++ server-py/routers/athlete_router.py
@@ -3,21 +3,25 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database import get_db
 from schemas.athlete_schemas import AthleteCreate, AthleteResponse
-from controllers.athlete_controller import create_athlete, get_athletes
+from controllers.athlete_controller import create_athlete, get_athletes, delete_athlete
 from dependencies.auth import get_current_user
 from models import User
 
+# Athlete Router
+# Prefix prefix="/athlete"
 router = APIRouter()
 
 @router.post("/create", response_model=AthleteResponse)
-def create(athlete_info: AthleteCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
-    """Create a new athlete            
-    """
-    print(athlete_info)
-    return create_athlete(athlete_info, db, current_user)
+async def create(athlete_info: AthleteCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    """Create a new athlete"""
+    return await create_athlete(athlete_info, db, current_user)
 
 @router.get("/", response_model=List[AthleteResponse])
-def get(db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+async def get(db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     """Get all athletes"""
-    
-    return get_athletes(db, current_user)
+    return await get_athletes(db, current_user)
+
+@router.delete("/{id}")
+async def get(id, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    """Delete athlete of id provided in path parameter"""
+    return await delete_athlete(db, current_user, id)
