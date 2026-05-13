aba24d887adb77beb525501147fcf220bf3a6f12
Bryan_w11 <h.bryan68@yahoo.ca>
2026-05-13 17:18:49 -0400

    added create user method

--- server-py/repositories/user_repository.py
+++ server-py/repositories/user_repository.py
@@ -3,8 +3,15 @@ from sqlalchemy import select
 from pydantic import EmailStr
 from models import User
 
+
 class UserRepository:
 
-        async def get_by_email(db: AsyncSession, email: EmailStr) -> User | None:
-                results = await db.execute(select(User).filter(User.email == email))
-                return results.scalars().first()
+    async def get_by_email(db: AsyncSession, email: EmailStr) -> User | None:
+        results = await db.execute(select(User).filter(User.email == email))
+        return results.scalars().first()
+
+    async def create_user(db: AsyncSession, new_user: User) -> User:
+        # Insert new User into database
+        db.add(new_user)
+        await db.commit()
+        await db.refresh(new_user)
