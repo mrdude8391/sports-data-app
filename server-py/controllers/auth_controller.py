3eac595b5be4d5250b55c511769efec86f97aa74
Bryan_w11 <h.bryan68@yahoo.ca>
2026-05-13 15:14:27 -0400

    formatting

--- server-py/controllers/auth_controller.py
+++ server-py/controllers/auth_controller.py
@@ -22,19 +22,22 @@ load_dotenv()
 password_hash = PasswordHash.recommended()
 TOKEN_EXPIRE_TIME = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
+
 def verify_password(plain_password, hashed_password):
     """
     Verifies if provided password matches a given hash.
     """
     return password_hash.verify(plain_password, hashed_password)
 
+
 def get_password_hash(password):
     """Hashes a password using the current hash"""
     return password_hash.hash(password)
 
+
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     """Returns an encoded JWT
-    
+
     data: Requires a dict / object{}
     expires_delta: a timedelta object
     """
@@ -44,46 +47,40 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     else:
         expire = datetime.now(timezone.utc) + timedelta(hours=TOKEN_EXPIRE_TIME)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, os.getenv("JWT_SECRET"), algorithm=os.getenv("ALGORITHM"))
+    encoded_jwt = jwt.encode(
+        to_encode, os.getenv("JWT_SECRET"), algorithm=os.getenv("ALGORITHM")
+    )
     return encoded_jwt
+
 
 async def register_user(user_data: RegisterPayload, db: AsyncSession) -> UserWithToken:
     """
     Create a new user row using provided information in the database
     """
-    try:
-        # Check if user already exists
-        existing_user = await UserRepository.get_by_email(db, user_data.email)
-        if existing_user:
-            raise DuplicateUserError
-            
-        # Hash the password
-        hashed_password = get_password_hash(user_data.password)
+    # Check if user already exists
+    existing_user = await UserRepository.get_by_email(db, user_data.email)
+    if existing_user:
+        raise DuplicateUserError
 
-        # Create new user
-        new_user = User(
-            username=user_data.username,
-            email=user_data.email,
-            password=hashed_password,
-        )
+    # Hash the password
+    hashed_password = get_password_hash(user_data.password)
+    # Create new user
+    new_user = User(
+        username=user_data.username,
+        email=user_data.email,
+        password=hashed_password,
+    )
+    # Insert new User into database
+    db.add(new_user)
+    await db.commit()
+    await db.refresh(new_user)
+    # Generate token
+    access_token_expires = timedelta(hours=TOKEN_EXPIRE_TIME)
+    token = create_access_token({"id": str(new_user.id)}, access_token_expires)
+    return UserWithToken(
+        id=new_user.id, username=new_user.username, email=new_user.email, token=token
+    )
 
-        # Insert new User into database
-        db.add(new_user)
-        await db.commit()
-        await db.refresh(new_user)
-
-        # Generate token
-        access_token_expires = timedelta(hours=TOKEN_EXPIRE_TIME)
-        token = create_access_token({"id" : str(new_user.id)}, access_token_expires)
-
-        return UserWithToken(
-            id= new_user.id,
-            username= new_user.username,
-            email= new_user.email,
-            token=token
-        )
-    except ValueError as err:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Registration Failed")
 
 async def login_user(login_payload: LoginPayload, db: AsyncSession) -> UserWithToken:
     """
@@ -101,11 +98,10 @@ async def login_user(login_payload: LoginPayload, db: AsyncSession) -> UserWithT
         raise InvalidCredentialsError
     # Generate token
     access_token_expires = timedelta(hours=TOKEN_EXPIRE_TIME)
-    token = create_access_token({"id" : str(existing_user.id)}, access_token_expires)
+    token = create_access_token({"id": str(existing_user.id)}, access_token_expires)
     return UserWithToken(
         id=existing_user.id,
         username=existing_user.username,
         email=existing_user.email,
         token=token,
     )
-    
