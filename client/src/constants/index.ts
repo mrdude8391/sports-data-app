8c7c21a97c481f3d8b8e6b6a776416e5f61f1a28
Bryan_w11 <h.bryan68@yahoo.ca>
2026-05-14 13:37:11 -0400

    removed unused comments

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -11,45 +11,6 @@ export const NAV_LINKS_LOGGED_OUT = [
   { href: "/about", key: "About", label: "About" },
 ];
 
-// export const STAT_FIELDS = {
-//     attack: [
-//     { key: "kills", label: "Kills" },
-//     { key: "errors", label: "Attack Errors" },
-//     { key: "total", label: "Total Attacks" },
-//     { key: "percentage", label: "Hitting %"},
-//   ],
-//   setting: [
-//     { key: "assists", label: "Assists" },
-//     { key: "errors", label: "Setting Errors" },
-//     { key: "attempts", label: "Setting Attempts" },
-
-//   ],
-//   serving: [
-//     { key: "rating", label: "Serve Rating" },
-//     { key: "aces", label: "Aces" },
-//     { key: "errors", label: "Serve Errors" },
-//     { key: "attempts", label: "Serve Attempts" },
-//     { key: "percentage", label: "Serving Percentage" },
-//   ],
-//   passing: [
-//     { key: "rating", label: "Passing Rating" },
-//     { key: "errors", label: "Passing Errors" },
-//     { key: "attempts", label: "Passing Attempts" },
-//   ],
-//   defense : [
-//     { key: 'digs', label: "Digs"}
-//   ],
-//   blocking : [
-//     { key: "total", label: "Total Blocks" },
-//     { key: "kills", label: "Block Kills" },
-//     { key: "solos", label: "Solo Blocks" },
-//     { key: "goodTouches", label: "Good Block Touches" },
-//     { key: "attempts", label: "Block Attempts" },
-//     { key: "errors", label: "Block Errors" },
-//   ]
-
-// }
-
 export const STAT_INDEX = [
   {
     category: "attack",
