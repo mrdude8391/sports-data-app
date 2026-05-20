739058a91a964c720a0f09709c270ad173ca2716
Bryan_w11 <h.bryan68@yahoo.ca>
2026-05-20 18:45:22 -0400

    moved configuration logic out

--- client/src/services/sportsDataService.ts
+++ client/src/services/sportsDataService.ts
@@ -1,6 +1,5 @@
 import type { Athlete, NewAthlete } from "@/features/athletes/types/Athlete";
 
-import axios, { AxiosError, type InternalAxiosRequestConfig } from "axios";
 import type {
   LoginPayload,
   User,
@@ -12,63 +11,11 @@ import type {
   AthleteStatResponse,
   Stat,
 } from "@/features/stats/types/Stat";
-
-export interface ApiError {
-  message: string;
-  error: string;
-  status: number;
-}
-
-const API_URL = import.meta.env.VITE_API_URL;
-
-const api = axios.create({
-  baseURL: API_URL,
-});
-
-let logoutCallback: () => void | null;
-
-export const setLogoutCallback = (cb: () => void) => {
-  logoutCallback = cb;
-};
-
-api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
-  if (config.headers) {
-    config.headers.Accept = "application/json";
-  }
-  config.withCredentials = true;
-  const savedUser = localStorage.getItem("user");
-  if (savedUser) {
-    const user: User = JSON.parse(savedUser);
-    config.headers.Authorization = `Bearer ${user.token}`;
-  }
-  return config;
-});
-
-api.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error: AxiosError<ApiError>) => {
-    if (error.response && error.status === 401) {
-      // console.log("Error 401: Unauthorized token. Clearing local storage token")
-      logoutCallback();
-    }
-    console.log(error);
-    const normalizedError = {
-      message: error.response?.data?.message ?? "Unknown error",
-      error: error.response?.data?.error ?? "UNKNOWN_ERROR",
-      status: error.response?.status,
-    };
-
-    return Promise.reject(normalizedError);
-  },
-);
+import { api } from "@/lib/api";
 
 export const test = async () => {
   try {
     const res = await api.get("/test");
-
-    console.log("API Test Service", API_URL);
     return res;
   } catch (error: any) {
     console.log("API Test Service", error);
