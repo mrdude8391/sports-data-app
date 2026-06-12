e3d63482c1d431008b3b8780505e2216fe090bd5
Bryan_w11 <h.bryan68@yahoo.ca>
2026-06-12 13:09:05 -0400

    formatting

--- server-py/src/athletes/repository.py
+++ server-py/src/athletes/repository.py
@@ -5,13 +5,13 @@ from sqlalchemy import select
 from ..athletes.models import Athlete
 
 
-async def get_athletes_by_user_id(userId: uuid.UUID, db: AsyncSession) -> List[Athlete]:
-    results = await db.execute(select(Athlete).where(Athlete.user_id == userId))
-    return results.scalars().all()
-
-
 async def create_new_athlete(new_athlete: Athlete, db: AsyncSession) -> Athlete:
     db.add(new_athlete)
     await db.flush()
     await db.refresh(new_athlete)
     return new_athlete
+
+
+async def get_athletes_by_user_id(userId: uuid.UUID, db: AsyncSession) -> List[Athlete]:
+    results = await db.execute(select(Athlete).where(Athlete.user_id == userId))
+    return results.scalars().all()
