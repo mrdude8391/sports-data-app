bb6e944a92a065426e39c9e684785436b2d3e54d
Bryan_w11 <h.bryan68@yahoo.ca>
2026-07-08 12:25:08 -0400

    change retry to just once

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,7 +7,13 @@ import { ThemeProvider } from "./context/ThemeProvider.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Create a client
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
